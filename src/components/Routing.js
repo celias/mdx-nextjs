2d6018a46e9ef04619f01de869c1fb789b87a4e7
Gabriella DiVentura <gabrielladiv@gmail.com>
2019-06-18 15:45:20 -0400

    Slides done

---
+++ src/components/Routing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Card } from "rebass";
+
+function Routing() {
+  return (
+    <Box>
+      <Card>
+        <h4>It's Routing is stupid simple.</h4>
+      </Card>
+    </Box>
+  );
+}
+
+export default Routing;
